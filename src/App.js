cbf152a13bc0d1a5df86718f77ff420e45a04971
Lucas Curtolo Belem <lucasc.belem@gmail.com>
2025-10-26 22:37:48 -0300

    Ajustando navegação de telas

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import Home from "./pages/HomePage/home";
 import Login from "./pages/Login/login";
 import Cadastro from "./pages/Cadastrar/cadastrar";
 import Verificar_cod from "./pages/Verificar_cod/verificar_cod";
+import Cadastrar_produto from "./pages/Cadastrar_Produto/cadastrar_produto";
+import ListarProduto from "./pages/ListarProdutos/listarprodutos";
 
 function App() {
   return (
@@ -14,6 +16,8 @@ function App() {
         <Route path="/login" element={<Login />} />
         <Route path="/cadastro" element={<Cadastro />} />
         <Route path="/verificar_cod" element={<Verificar_cod />} />
+        <Route path="/cadastrar_Produto" element={<Cadastrar_produto />} />
+        <Route path="/listar_produtos" element={<ListarProduto />} />
       </Routes>
     </Router>
   );
